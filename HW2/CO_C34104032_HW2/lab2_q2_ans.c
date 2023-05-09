8ed63627350f5bf82e8c4aa84dd19b771c903a8a
kuanyi0226 <kuanyi0226@gmail.com>
2023-05-09 22:41:01 +0800

    tiny fix

--- HW2/CO_C34104032_HW2/lab2_q2_ans.c
+++ HW2/CO_C34104032_HW2/lab2_q2_ans.c
@@ -1,6 +1,7 @@
 // TODO : 
 "addi t1, x0, 8\n\t"//length of x[8] 
 "addi t2, %[p_x], 14\n\t"//address of x[8] 
+"addi t5, x0, -1\n\t" //for judge jump to SETflag
 
 "loop2:\n\t"
 "vsetvli t0, t1, e16, ta, ma\n\t"
@@ -12,9 +13,8 @@
 "vadd.vx v1, v0, t3\n\t" //v1 = v0 + t3
 "vmseq.vx v2, v1, %[target]\n\t" //=flag?
 
-"vse16.v v2, (%[p_tmp1])\n\t"
-"lh t4, 0(%[p_tmp1])\n\t"
-"bne t4, x0, SETflag\n\t" //check whether the flag is set
+"vfirst.m t4, v2\n\t" //find 1 then set 1, else set -1
+"bne t4, t5, SETflag\n\t" //check whether the flag is set
 
 "bge t2, %[p_x], loop2\n\t" //jump back to loop2
 "beq x0, x0, EXIT\n\t" //directly jump to EXIT
